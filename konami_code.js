6a8ee9dfe04379d81080db56218fd97efc3425a7
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-26 00:02:26 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,16 +2,12 @@ const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
     let index = 0;
-  //const body = document.querySelector('body');
-    document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which);
-    debugger
+    document.addEventListener('keydown', function(e) {
+      const key = parseInt(e.detail || e.which);
       if (key === alphabet[index]) {
         index++;
-
         if (index === alphabet.length) {
-          window.alert("Hurray!");
-
+          alert("Wooooo!");
           index = 0;
         }
       } else {
